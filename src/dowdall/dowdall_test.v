7bc975a35fdc9f1468578424e9e4f78c693ab11e
rakazv <rakazv@outlook.com>
2024-07-30 18:10:24 +0900

    up

--- src/dowdall/dowdall_test.v
+++ src/dowdall/dowdall_test.v
@@ -9,7 +9,7 @@ fn test_dowdall() {
 	assert result[1].name == 'Asan'
 }
 
-fn test_invalid_point() {
+fn test_invalid_rank() {
 	mut dowdall := new(['Asan', 'Bsan', 'Csan', 'Dsan'])
 	dowdall.vote([100, 3, 2, 1]) or {
 		assert true
